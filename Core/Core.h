ad94bfecc6f6f5162e6acc0f950dbab6e1711974
Gabidal <Quanfoxes@gmail.com>
2022-02-04 00:16:31 +0200

    .

--- Core/Core.h
+++ Core/Core.h
@@ -26,7 +26,7 @@ public:
 
 		for (int X = 0; X < World_Size; X++) {
 			for (int Z = 0; Z < World_Size; Z++) {
-				Chunks[X, Z] = new Chunk();
+				Chunks[X, Z] = new Chunk(Pack_Patterns(X, Z));
 			}
 		}
 	}
@@ -37,14 +37,31 @@ public:
 		return Patterns.back().Color + 1;
 	}
 
-	vector<Pattern*> Pack_Patterns() {
+	/// <summary>
+	/// Depending on the X & Z, this function will populate a list
+	/// With amount of function patterns, depending on the population density-
+	/// that Smart_Counter gives
+	/// </summary>
+	vector<Pattern*> Pack_Patterns(int X, int Z) {
+		int Range = Patterns.size();
 
+		int Population = Smart_Counter::Count(X, Z, Range);
+
+		vector<Pattern*> Result;
+		Result.resize(Population);
+		
+		for (int i = 0; i < Patterns.size(); i++) {
+			Result[i] = &Patterns[i];
+		}
 	}
 };
 
+
 namespace Smart_Counter {
+	// Save information of all other counters, for different ranges.
 	map<int, siv::BasicPerlinNoise<float>*> Counters;
 
+	//this is to keep the pattern population natural in paralell X & Z
 	int Count(int X, int Z, int Range) {
 		siv::BasicPerlinNoise<float>* Counter = Counters[Range];
 		if (Counter == nullptr) {
